ef1a891aebd25c080746716cd4dfb12c48f1f9f6
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-22 16:38:24 +0530

    Create revs.java

---
+++ revs.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+public class revs{
+      public static void main(String aa[]){
+          String s;
+          Scanner m=new Scanner(System.in);
+          s=m.next();
+          String n=new StringBuffer(s).reverse().toString();
+          System.out.println(n);
+}
+}
