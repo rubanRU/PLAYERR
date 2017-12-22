2fb8e50a9469083fefd98cbc841a52efe50d861e
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-22 16:46:46 +0530

    Create parse.java

---
+++ parse.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+public class parse{
+      public static void main(String aa[]){
+          String p;
+          Scanner m=new Scanner(System.in);
+          p=m.next();
+          int s=Integer.parseInt(p);
+          System.out.println(s);
+}
+}
