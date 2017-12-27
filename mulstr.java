346b979fe6bf83f3f0b0de42c3db0bd7b6daa1d6
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-27 20:37:34 +0530

    Create mulstr.java

---
+++ mulstr.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class mulstr {
+    public static void main(String aa[]){
+   int n;
+   Scanner s=new Scanner(System.in);
+   String a=s.next();
+   String b=s.next();
+   int a1=Integer.parseInt(a);
+   int b1=Integer.parseInt(b);
+   int c=a1*b1;
+   String d=String.valueOf(c);
+   System.out.print(d);
+
+}
+}
+
