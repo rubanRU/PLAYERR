2e2ee417d7fd817326169e249437baa79a97ea35
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-28 18:30:34 +0530

    Create diff1.java

---
+++ diff1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class diff1{
+    public static void main(String aa[]){
+    int n;
+    int c=0;
+    int h=0;
+    int sum=0;
+    Scanner s=new Scanner(System.in);
+    n=s.nextInt();
+    for(int i=0;i<n;i++){
+        for(int j=0;i<n;j++){
+            if(i-j==1){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
+}
+
