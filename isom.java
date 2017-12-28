a16cd7fa9add13b1c9cb37e4f6dd98e8c5215543
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-28 20:06:00 +0530

    Create isom.java

---
+++ isom.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+
+public class isom{
+
+    public static void main(String aa[]) {
+        String a;
+        String b;
+        char k;
+        int f=0;
+        char v;
+        Scanner s = new Scanner(System.in);
+        a = s.next();
+        b = s.next();
+        HashMap<Character, Character> p = new HashMap<Character, Character>();
+        int l = a.length();
+        for (int i = 0; i < l; i++) {
+            k = a.charAt(i);
+            v = b.charAt(i);
+            p.put(k, v);
+
+        }
+        for (int i = 0; i < l; i++) {
+            k = a.charAt(i);
+            v = b.charAt(i);
+            char t = p.get(k);
+            if (p.containsKey(k)) {
+                if (!p.containsValue(v)) {
+                    System.out.println("False");
+                    f=1;
+                }
+            }
+
+        }
+        if(f==0){
+            System.out.println("True");
+        }
+    }
+}
