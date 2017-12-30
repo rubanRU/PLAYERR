df6ca036244e715cc67a05a43f842b9ec0b00d6b
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-30 11:19:30 +0530

    Create rems.java

---
+++ rems.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class rems{
+    public static void main(String aa[]){
+    Scanner s=new Scanner(System.in);
+    String c=s.next();
+    String h=s.next();
+    String re=s.next();
+    String k=h.replace(re, "");
+    String l=h.replace(re, "");
+    System.out.println(k);
+    System.out.println(l);
+   
+}
+}
