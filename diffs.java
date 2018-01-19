a78afb95a63209320bf39c5859cc35a21b26a0f7
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-01-19 21:12:20 +0530

    Create diffs.java

---
+++ diffs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class diffs{
+    public static void main(String aa[]){
+        String a,b;
+        Scanner s=new Scanner(System.in);
+        a=s.next();
+        b=s.next();
+        int kk=s.nextInt();
+        StringBuffer xx=new StringBuffer(a);
+        int m=xx.length();
+        xx.insert(0, '[');
+        xx.insert(m+1, ']');
+        String c=xx.toString();
+        String z=b.replaceAll(b, "");
+        System.out.print(z);
+        if(z.length()==kk){
+            System.out.println("Yes");
+        }
+ else{
+            System.out.print("No");
+ }
+    }
+}
