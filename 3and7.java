937822d91527bdd4c885ac3bc99b6b185e5dda7a
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-01-21 20:52:37 +0530

    Create 3and7.java

---
+++ 3and7.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class 3and7{
+    public static void main(String aa[]){
+         int n;
+         int a=0;
+         int b=0;
+         Scanner s=new Scanner(System.in);
+         n=s.nextInt();
+         int g=n;
+        
+         if((n%3==0)||(n%7==0)){
+            System.out.print("Yes");
+         }
+ else{
+             System.out.println("no");
+ }
+
+    }
+
+}
