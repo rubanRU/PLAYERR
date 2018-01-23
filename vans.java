aca6304e8c64e408c8dfd550d526d209258156ff
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-01-23 20:55:09 +0530

    Create vans.java

---
+++ vans.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class vans {
+    public static void main(String aa[]){
+       Scanner z=new Scanner(System.in);
+       String c=z.nextLine();
+       if(c.contains("Vishal")&&c.contains("Sundar")){
+           System.out.print("yes");
+       }
+      
+      
+
+
+
+     
+       
+    }
+}
