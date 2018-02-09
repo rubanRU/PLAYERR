9c7c51a3b3ed6460d8f7b338d275486b3c281d11
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-02-09 21:53:56 +0530

    Create subb.java

---
+++ subb.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class subb {
+  public static void main(String aa[]){
+      int n;
+      Scanner s=new Scanner(System.in);
+      String a=s.next();
+      String h=s.next();
+      if(a.contains(h)){
+          System.out.println("yes");
+      }
+ else{
+          System.out.println("No");
+ }
+      }
+    }
+
