d7d7fe8a39eba540f5a702c04ee58732d81abc2c
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-02-16 20:59:14 +0530

    Create spac.java

---
+++ spac.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class spac{
+public static void main(String a[]){
+String f;
+Scanner s=new Scanner(System.in);
+f=s.nextLine();
+String b[]=f.split(" ");
+for(int i=0;i<b.length;i++){
+ System.out.print(b[i]);
+}
+}
+}
