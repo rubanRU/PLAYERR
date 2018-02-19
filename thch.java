9be55c11fa138d3aa39b18474f0de4a647d2f4de
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-02-19 20:59:35 +0530

    Create thch.java

---
+++ thch.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.ArrayList;
+public class thch {
+  public static void main(String aa[]){
+      int n;
+      int f=0;
+      Scanner s=new Scanner(System.in);
+      String g=s.nextLine();
+      for(int i=2;i<g.length();i+=3){
+          System.out.print(g.charAt(i));
+      }
+
+}
+}
