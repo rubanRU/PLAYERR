591c729ad513dd91becaf05c956a839906c07555
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-03-01 18:42:23 +0530

    Rename spac.java to spac1.java

---
+++ spac1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class spa {
+  public static void main(String aa[]){
+     String a;
+      Scanner s=new Scanner(System.in);
+      a=s.nextLine();
+      String h[]=a.split(" ");
+      for(int i=0;i<h.length;i++){
+          System.out.print(h[i]+" ");
+      }
+
+    }
+}
