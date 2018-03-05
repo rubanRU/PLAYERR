857276cae1b0d1cbcbb68dd01f1b5585425c06a1
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-03-05 21:54:28 +0530

    Create bitn.java

---
+++ bitn.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.ArrayList;
+public class bitn{
+  public static void main(String aa[]){
+     Scanner s=new Scanner(System.in);
+     int f=s.nextInt();
+     int u=~f;
+     System.out.print(u);
+}
+}
