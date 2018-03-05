e61e42b702d03d2ab2b217789d73b67db0edd893
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-03-05 22:05:25 +0530

    Create shi.java

---
+++ shi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.ArrayList;
+public class shi{
+  public static void main(String aa[]){
+     Scanner s=new Scanner(System.in);
+     int f=s.nextInt();
+     int u=s.nextInt();
+     int k=f<<u;
+     
+     System.out.print(k);
+
+}
+}
