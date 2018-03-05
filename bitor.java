ff4ad449c81c4f398d86ad4e99610ea729462d42
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-03-05 21:46:50 +0530

    Create bitor.java

---
+++ bitor.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.ArrayList;
+public class bitor{
+  public static void main(String aa[]){
+     Scanner s=new Scanner(System.in);
+     int f=s.nextInt();
+     int b[]=new int[f];
+     ArrayList<Integer>a=new ArrayList<Integer>();
+     for(int i=0;i<f;i++){
+         b[i]=s.nextInt();
+     }
+     for(int i=0;i<f;i++){
+         for(int j=0;j<f;j++){
+             int y=b[i]|b[j];
+             a.add(y);
+         }
+     }
+     System.out.println(Collections.max(a));
+}
+}
