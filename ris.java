696b07fae5917365d3009999ad8c6a0b3ff4b5f3
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-03-05 22:08:01 +0530

    Create ris.java

---
+++ ris.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.ArrayList;
+public class ris{
+  public static void main(String aa[]){
+     Scanner s=new Scanner(System.in);
+     int f=s.nextInt();
+     int u=s.nextInt();
+     float k=f>>u;
+     
+     System.out.print(k);
+
+}
+}
