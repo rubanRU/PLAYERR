5c852a6ec86f3ff736460be9c367ac320b88f635
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-03-05 21:50:45 +0530

    Create bitorr.java

---
+++ bitorr.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.ArrayList;
+public class bitorr{
+  public static void main(String aa[]){
+     Scanner s=new Scanner(System.in);
+     int f=s.nextInt();
+     int y=s.nextInt();
+     int u=f|y;
+     System.out.print(u);
+}
+}
